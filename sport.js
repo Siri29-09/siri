d6a13fcb6d8ef91ceedc494de8c2f24ecb378081
Siri29-09 <shirishapulidindi@gmail.com>
2024-04-01 23:00:40 +0530

    adding changes

---
+++ sport.js
@@ -0,0 +1,6 @@
+let input=document.querySelector("#inpt");
+let button=document.querySelector("#btn-1");
+button.addEventListener("click",fn);
+function fn() {
+    document.getElementById("btn-1").innerHTML = "Hello World";
+  }

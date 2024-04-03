cf243b719a374e1302a786985b98ce0afc3ff621
Siri29-09 <shirishapulidindi@gmail.com>
2024-04-03 16:34:20 +0530

    removing comments

--- sport.js
+++ sport.js
@@ -1,14 +1,10 @@
 let Btn1=document.querySelector("#but-1");
 let taskInput=document.querySelector("#task-input");
 
-//  Btn1.addEventListener("click",()=>{
-//    document.getElementById("task-list").innerHTML=taskInput.innerHTML="";
-//  })
+
 
 function myFunction(callback) {
-  // Get the value of the input field with id="numb"
   let text = document.getElementById("task-input").value;
-  // If x is Not a Number or less than one or greater than 10
   
 callback(text )
 

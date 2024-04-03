45e0879d77974e2430caaa8dadcd76a10592ff9f
Siri29-09 <shirishapulidindi@gmail.com>
2024-04-03 16:33:35 +0530

    adding new tasks

--- sport.js
+++ sport.js
@@ -1,6 +1,28 @@
-let input=document.querySelector("#inpt");
-let button=document.querySelector("#btn-1");
-button.addEventListener("click",fn);
-function fn() {
-    document.getElementById("btn-1").innerHTML = "Hello World";
-  }
+let Btn1=document.querySelector("#but-1");
+let taskInput=document.querySelector("#task-input");
+
+//  Btn1.addEventListener("click",()=>{
+//    document.getElementById("task-list").innerHTML=taskInput.innerHTML="";
+//  })
+
+function myFunction(callback) {
+  // Get the value of the input field with id="numb"
+  let text = document.getElementById("task-input").value;
+  // If x is Not a Number or less than one or greater than 10
+  
+callback(text )
+
+  
+}
+
+Btn1.addEventListener("click",()=>{
+  myFunction((text)=>{
+   const ul = document.getElementById("task-list")
+   const li = document.createElement("li")
+   const content = document.createTextNode(text)
+   li.append(content)
+   ul.appendChild(li)
+   document.getElementById("task-input").value=""
+  })
+})
+

36fb8b07c0cd5ee39dd53e67aa66d45e56640957
Siri29-09 <shirishapulidindi@gmail.com>
2024-04-05 04:59:41 +0530

    changed the layout in html file

--- sport.js
+++ sport.js
@@ -1,24 +1,40 @@
 let Btn1=document.querySelector("#but-1");
 let taskInput=document.querySelector("#task-input");
-
-
-
-function myFunction(callback) {
-  let text = document.getElementById("task-input").value;
-  
-callback(text )
-
-  
-}
+let taskList=document.querySelector("#task-list");
 
 Btn1.addEventListener("click",()=>{
-  myFunction((text)=>{
-   const ul = document.getElementById("task-list")
-   const li = document.createElement("li")
-   const content = document.createTextNode(text)
-   li.append(content)
-   ul.appendChild(li)
-   document.getElementById("task-input").value=""
-  })
+  if(taskInput.value !==""){
+    const taskText=taskInput.value;
+    const listItem=document.createElement('li')
+    listItem.innerHTML=` ${taskText}  <button type="button" class="Delete-btn btn-outline-danger ms-5"><i class="fa-solid fa-xmark"></i></button>`;
+    taskList.appendChild(listItem);
+    taskInput.value="";
+  }
+})
+taskList.addEventListener('click',(event)=>{
+     if(event.target.classList.contains("Delete-btn")){
+      const listItem=event.target.parentElement;
+      taskList.removeChild(listItem);
+     }
 })
 
+
+// function myFunction(callback) {
+//   let text = document.getElementById("task-input").value;
+  
+// callback(text)
+
+  
+// }
+
+// Btn1.addEventListener("click",()=>{
+//   myFunction((text)=>{
+//    const ulList = document.getElementById("task-list");
+//    const list = document.createElement("li");
+//    const content = document.createTextNode(text);
+//     list.append(content);
+//    ulList.appendChild(list);
+//    document.getElementById("task-input").value="";
+//   })
+// })
+// // 
